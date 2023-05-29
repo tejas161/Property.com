b21240c87fb7c848281faef07933e73e4bb4a913
tejas161 <tejasgawankar16@gmail.com>
2023-05-29 21:17:59 +0530

    userprofile

--- client/src/components/UserIcon.js
+++ client/src/components/UserIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import {  List,ListItem } from '@mui/material'
+import { List, ListItem } from '@mui/material'
 
 
 import Avatar from '@mui/material/Avatar';
@@ -15,247 +15,350 @@ import Avatar from '@mui/material/Avatar';
 //   import PopperUnstyled from '@mui/base/PopperUnstyled';
 
 
-import MenuItem, { menuItemClasses } from '@mui/base/MenuItem';
+// import MenuItem, { menuItemClasses } from '@mui/base/MenuItem';
 
-import Popper from '@mui/base/Popper';
+// import Popper from '@mui/base/Popper';
 
-  import { styled } from '@mui/system';
+import { styled } from '@mui/system';
 
 
-  const blue = {
-    100: '#DAECFF',
-    200: '#99CCF3',
-    400: '#3399FF',
-    500: '#007FFF',
-    600: '#0072E5',
-    900: '#003A75',
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+const blue = {
+  100: '#DAECFF',
+  200: '#99CCF3',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  900: '#003A75',
+};
+
+const grey = {
+  50: '#f6f8fa',
+  100: '#eaeef2',
+  200: '#d0d7de',
+  300: '#afb8c1',
+  400: '#8c959f',
+  500: '#6e7781',
+  600: '#57606a',
+  700: '#424a53',
+  800: '#32383f',
+  900: '#24292f',
+};
+
+// const StyledListbox = styled('ul')(
+//   ({ theme }) => `
+//   font-family: IBM Plex Sans, sans-serif;
+//   font-size: 0.875rem;
+//   box-sizing: border-box;
+//   padding: 6px;
+//   margin: 12px 0;
+//   min-width: 200px;
+//   border-radius: 12px;
+//   overflow: auto;
+//   outline: 0px;
+//   background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+//   border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+//   color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+//   box-shadow: 0px 4px 30px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
+//   `,
+// );
+
+// const StyledMenuItem = styled(MenuItem)(
+//   ({ theme }) => `
+//   list-style: none;
+//   padding: 8px;
+//   border-radius: 8px;
+//   cursor: pointer;
+//   user-select: none; 
+
+
+
+//   &:last-of-type {
+//     border-bottom: none;
+//   }
+
+//   &.${menuItemClasses.focusVisible} {
+//      outline: 3px solid ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+//        outline:none;
+//      background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+//     color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+//   }
+
+//   &.${menuItemClasses.disabled} {
+//     color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+//   }
+
+//   &:hover:not(.${menuItemClasses.disabled}) {
+//     background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+//     color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+//   }
+//   `,
+// );
+
+
+
+//   const PopperStyled = styled(Popper)`
+//   z-index: 1;
+// `;
+
+
+
+// const UserIcon = ({name,profileIcon}) => {
+
+//     const [anchorEl, setAnchorEl] = React.useState(null);
+//     const isOpen = Boolean(anchorEl);
+//     const buttonRef = React.useRef(null);
+//     const menuActions = React.useRef(null);
+//     const preventReopen = React.useRef(false);
+
+
+
+//     const handleButtonClick = (event) => {
+//       if (preventReopen.current) {
+//         event.preventDefault();
+//         preventReopen.current = false;
+//         return;
+//       }
+
+//       if (isOpen) {
+//         setAnchorEl(null);
+//       } else {
+//         setAnchorEl(event.currentTarget);
+//       }
+//     };
+
+//     const handleButtonMouseDown = () => {
+//       if (isOpen) {
+//         // Prevents the menu from reopening right after closing
+//         // when clicking the button.
+//         preventReopen.current = true;
+//       }
+//     };
+
+//     const handleButtonKeyDown = (event) => {
+//       if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+//         event.preventDefault();
+//         setAnchorEl(event.currentTarget);
+//         if (event.key === 'ArrowUp') {
+//           menuActions.current?.highlightLastItem();
+//         }
+//       }
+//     };
+
+//     const close = () => {
+//       setAnchorEl(null);
+//       buttonRef.current.focus();
+//     };
+
+//     const createHandleMenuClick = (menuItem) => {
+//       return () => {
+//         console.log(`Clicked on ${menuItem}`);
+//         close();
+//       };
+//     };
+
+
+
+
+//     return(
+//         <>
+
+//        <Avatar
+//          onClick={handleButtonClick}
+//          onKeyDown={handleButtonKeyDown}
+//          onMouseDown={handleButtonMouseDown}
+//          ref={buttonRef}
+//          aria-controls={isOpen ? 'simple-menu' : undefined}
+//          aria-expanded={isOpen || undefined}
+//          aria-haspopup="menu"
+//          src={profileIcon ? profileIcon : ""}
+
+//         sx={{ bgcolor: 'orange', cursor: 'pointer',marginRight:'10px' }}>{name  
+//           ? name[0].toUpperCase() : 'U'
+
+
+//         }</Avatar>
+
+
+//     <MenuItem
+//         actions={menuActions}
+//         open={isOpen}
+//         onClose={close}
+//         anchorEl={anchorEl}
+//         slots={{ root: PopperStyled, listbox: StyledListbox }}
+//         slotProps={{ listbox: { id: 'simple-menu' } }}
+//         sx={{zIndex:'1200'}}
+
+//       >
+
+//          <Link to="/myprofile" style={{textDecoration:'none',color:'inherit'}}>
+//          <StyledMenuItem onClick={close}>
+
+//          My Profile
+
+
+
+//        </StyledMenuItem>
+
+//          </Link>
+
+//          <Link to="/saved" style={{textDecoration:'none',color:'inherit'}}>
+//          <StyledMenuItem onClick={close}>
+
+//          Saved Property
+
+
+
+//        </StyledMenuItem>
+
+//          </Link>
+
+//          <Link to="/requestpage" style={{textDecoration:'none',color:'inherit'}}>
+//          <StyledMenuItem onClick={close}>
+
+//          Dashboard
+
+
+
+//        </StyledMenuItem>
+
+//          </Link>
+
+
+
+
+
+
+//        <Link to="/logout" style={{textDecoration:'none',color:'inherit'}}>
+//         <StyledMenuItem onClick={close}>
+//          Logout
+//         </StyledMenuItem>
+//         </Link>
+
+
+//       </MenuItem>
+
+
+
+
+
+
+
+
+
+//         </>
+
+
+
+
+
+//     );
+// }
+
+
+const UserIcon = ({ name, profileIcon }) => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-  
-  const grey = {
-    50: '#f6f8fa',
-    100: '#eaeef2',
-    200: '#d0d7de',
-    300: '#afb8c1',
-    400: '#8c959f',
-    500: '#6e7781',
-    600: '#57606a',
-    700: '#424a53',
-    800: '#32383f',
-    900: '#24292f',
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
-  const StyledListbox = styled('ul')(
-    ({ theme }) => `
-    font-family: IBM Plex Sans, sans-serif;
-    font-size: 0.875rem;
-    box-sizing: border-box;
-    padding: 6px;
-    margin: 12px 0;
-    min-width: 200px;
-    border-radius: 12px;
-    overflow: auto;
-    outline: 0px;
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    box-shadow: 0px 4px 30px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
-    `,
-  );
-  
-  const StyledMenuItem = styled(MenuItem)(
-    ({ theme }) => `
-    list-style: none;
-    padding: 8px;
-    border-radius: 8px;
-    cursor: pointer;
-    user-select: none;
-
-    
-    
-   
-
-    &:last-of-type {
-      border-bottom: none;
-    }
-  
-    &.${menuItemClasses.focusVisible} {
-       outline: 3px solid ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
-         outline:none;
-       background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-      color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    }
-  
-    &.${menuItemClasses.disabled} {
-      color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
-    }
-  
-    &:hover:not(.${menuItemClasses.disabled}) {
-      background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-      color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    }
-    `,
-  );
 
 
+  return (
 
-  const PopperStyled = styled(Popper)`
-  z-index: 1;
-`;
+    <>
 
+      <Avatar
+       aria-controls={open ? 'basic-menu' : undefined}
+       aria-haspopup="true"
+       aria-expanded={open ? 'true' : undefined}
+       onClick={handleClick}
+        src={profileIcon ? profileIcon : ""}
 
-
-const UserIcon = ({name,profileIcon}) => {
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const isOpen = Boolean(anchorEl);
-    const buttonRef = React.useRef(null);
-    const menuActions = React.useRef(null);
-    const preventReopen = React.useRef(false);
-
-  
-  
-    const handleButtonClick = (event) => {
-      if (preventReopen.current) {
-        event.preventDefault();
-        preventReopen.current = false;
-        return;
-      }
-  
-      if (isOpen) {
-        setAnchorEl(null);
-      } else {
-        setAnchorEl(event.currentTarget);
-      }
-    };
-  
-    const handleButtonMouseDown = () => {
-      if (isOpen) {
-        // Prevents the menu from reopening right after closing
-        // when clicking the button.
-        preventReopen.current = true;
-      }
-    };
-  
-    const handleButtonKeyDown = (event) => {
-      if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
-        event.preventDefault();
-        setAnchorEl(event.currentTarget);
-        if (event.key === 'ArrowUp') {
-          menuActions.current?.highlightLastItem();
-        }
-      }
-    };
-  
-    const close = () => {
-      setAnchorEl(null);
-      buttonRef.current.focus();
-    };
-  
-    const createHandleMenuClick = (menuItem) => {
-      return () => {
-        console.log(`Clicked on ${menuItem}`);
-        close();
-      };
-    };
-
-  
-
-
-    return(
-        <>
-           
-       <Avatar
-         onClick={handleButtonClick}
-         onKeyDown={handleButtonKeyDown}
-         onMouseDown={handleButtonMouseDown}
-         ref={buttonRef}
-         aria-controls={isOpen ? 'simple-menu' : undefined}
-         aria-expanded={isOpen || undefined}
-         aria-haspopup="menu"
-         src={profileIcon ? profileIcon : ""}
-       
-        sx={{ bgcolor: 'orange', cursor: 'pointer',marginRight:'10px' }}>{name  
+        sx={{ bgcolor: 'orange', cursor: 'pointer', marginRight: '10px' }}>{name
           ? name[0].toUpperCase() : 'U'
-        
-        
-        }</Avatar>
-    
-{/*    
-    <MenuItem
-        actions={menuActions}
-        open={isOpen}
-        onClose={close}
+
+
+        }
+
+      </Avatar>
+      <Menu
+        id="basic-menu"
         anchorEl={anchorEl}
-        slots={{ root: PopperStyled, listbox: StyledListbox }}
-        slotProps={{ listbox: { id: 'simple-menu' } }}
-        sx={{zIndex:'1200'}}
-      
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
       >
-        
-         <Link to="/myprofile" style={{textDecoration:'none',color:'inherit'}}>
-         <StyledMenuItem onClick={close}>
-         
-         My Profile
-      
-       
-      
-       </StyledMenuItem>
-         
+
+
+
+
+          <Link to="/myprofile" style={{textDecoration:'none',color:'inherit'}}>
+          <MenuItem onClick={handleClose}>
+
+          My Profile
+
+
+
+        </MenuItem>
+
+          </Link>
+
+          <Link to="/saved" style={{textDecoration:'none',color:'inherit'}}>
+          <MenuItem onClick={handleClose}>
+
+          Saved Property
+
+
+
+        </MenuItem>
+
+          </Link>
+
+          <Link to="/requestpage" style={{textDecoration:'none',color:'inherit'}}>
+          <MenuItem onClick={handleClose}>
+
+          Dashboard
+
+
+
+        </MenuItem>
+
+          </Link>
+
+
+
+
+
+
+        <Link to="/logout" style={{textDecoration:'none',color:'inherit'}}>
+         <MenuItem onClick={handleClose}>
+          Logout
+         </MenuItem>
          </Link>
 
-         <Link to="/saved" style={{textDecoration:'none',color:'inherit'}}>
-         <StyledMenuItem onClick={close}>
-         
-         Saved Property
-      
-       
-      
-       </StyledMenuItem>
-         
-         </Link>
 
-         <Link to="/requestpage" style={{textDecoration:'none',color:'inherit'}}>
-         <StyledMenuItem onClick={close}>
-         
-         Dashboard
-      
-       
-      
-       </StyledMenuItem>
-         
-         </Link>
+         </Menu>
+
+
+
+
+    </>
+  )
 
 
 
 
 
-       
-       <Link to="/logout" style={{textDecoration:'none',color:'inherit'}}>
-        <StyledMenuItem onClick={close}>
-         Logout
-        </StyledMenuItem>
-        </Link>
-
-      
-      </MenuItem>
-    
-     
-                                              
-                                                   */}
-                                            
-        
-        
-        
-        
-        </>
-
-
-
-
-
-    );
 }
-
-
 
 export default UserIcon;
