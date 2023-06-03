1d2520294bc575816a94fa33d3917230fb3c0dfd
tejas161 <tejasgawankar16@gmail.com>
2023-06-03 15:25:07 +0530

    aws region

--- client/src/context.js
+++ client/src/context.js
@@ -14,9 +14,9 @@ const AppProvider = ({ children }) => {
 
 
 //This is for localhost
-    // const url = 'http://44.201.156.0:8080';
+    const url = 'http://44.202.11.199:3001';
 
-   const url = '/api';
+//    const url = '/api';
 
 
 // const url = 'http://localhost:3001';
